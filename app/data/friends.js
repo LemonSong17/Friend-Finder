eb58e0b50b7f719a4d81103f2c0b1100198023fa
Joe Allen <joe.allen9@gmail.com>
2019-09-11 15:46:45 -0500

    pushing updated files. Still need to work on connection

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Batman',
+		photo: 'https://theplaylist.net/wp-content/uploads/2018/05/the-dark-knight-batman-1200x520.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Superman',
+		photo: 'https://images.squarespace-cdn.com/content/v1/51b3dc8ee4b051b96ceb10de/1564036849353-15PWUF67JGCBKRNTDYS2/ke17ZwdGBToddI8pDm48kAt72yGFwHZjoxtmj75n0VMUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYy7Mythp_T-mtop-vrsUOmeInPi9iDjx9w8K4ZfjXt2dvp1wM0jvciobd5mvjBb-PkjbbxSYDSdt-BIyUswy_5eG6v6ULRah83RgHXAWD5lbQ/superman.jpg?format=1500w',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Spider-Man',
+		photo: 'https://i.kinja-img.com/gawker-media/image/upload/cgscaepo68nrowzete13.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Thor',
+		photo: 'https://cdn.britannica.com/73/182873-050-E1C686F4/Chris-Hemsworth-Thor-Thor-The-Dark-World.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Dr. Strange',
+		photo: 'https://cdn.vox-cdn.com/thumbor/Dj3DetdJigfiY-pS1F4ntLyzYS4=/0x0:1920x1079/920x613/filters:focal(1085x298:1391x604):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/64773806/ply_dr_strange_graded.0.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Captain America',
+		photo: 'https://fsmedia.imgix.net/11/91/65/66/aa08/4ef6/be12/964b648601b6/captain-america-the-winter-soldier.jpeg?rect=0%2C0%2C1920%2C960&auto=format%2Ccompress&w=650',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Deadpool',
+		photo: 'https://www.screengeek.net/wp-content/uploads/2019/08/deadpool-mcu.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'The Incredible Hulk',
+		photo: 'https://oyster.ignimgs.com/wordpress/stg.ign.com/2019/04/hulk.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Wonder Woman',
+		photo: 'https://cdn.vox-cdn.com/thumbor/2aZdbHG9Cu-rvgtNvT7z88ZOB6o=/0x0:1280x640/920x613/filters:focal(467x58:671x262):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/55115281/wonderwoman2.0.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Aquaman',
+		photo: 'https://s3-us-east-2.amazonaws.com/redefined/wp-content/uploads/2018/12/20020214/aquaman_warnerbros.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
